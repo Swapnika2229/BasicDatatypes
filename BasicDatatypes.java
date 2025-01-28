b5f4e49f720e5e416f7d1a993f84409622cd2e12
Swapnika Mohanty <swapnikamohanty@gmail.com>
2025-01-28 21:23:00 +0530

    Add files via upload

---
+++ BasicDatatypes.java
@@ -0,0 +1,20 @@
+public class BasicDatatypes {
+    public static void main(String[] args) {
+        byte byteValue = 10;
+        System.out.println("Byte value: " + byteValue);
+        short shortValue = 20;
+        System.out.println("Short value: " + shortValue);
+        int intValue = 30;
+        System.out.println("Int value: " + intValue);
+        long longValue = 40L;
+        System.out.println("Long value: " + longValue);
+        float floatValue = 50.5f;
+        System.out.println("Float value: " + floatValue);
+        double doubleValue = 60.5;
+        System.out.println("Double value: " + doubleValue);
+        char charValue = 'A';
+        System.out.println("Char value: " + charValue);
+        boolean booleanValue = true;
+        System.out.println("Boolean value: " + booleanValue);
+    }
+}
